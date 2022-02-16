00b445f21238a52a5314d02a49d449b5d3d21f0f
Raul <raulvsh@gmail.com>
2022-02-16 12:13:41 +0100

    Añadido ejercicio vídeo 5

---
+++ prueba1.py
@@ -0,0 +1,13 @@
+print("Hola mundo")
+
+#altura = 184
+"""altura = int(input("Introduzca altura: "))
+
+if altura >= 180:
+    print("Eres alto")
+else:
+    print("Eres bajo")
+"""
+
+num=1
+print(type(num))
---
+++ video5.py
@@ -0,0 +1,13 @@
+def recibir_datos():
+    return input("Introduzca su nombre: ")
+
+def saludo(nombre):
+    print("Buenos días " + nombre)
+    
+nombre = recibir_datos()
+
+saludo(nombre)
+
+
+
+
