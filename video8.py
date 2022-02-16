b33d4e445af4476572bad7dc164376a205664c8d
Raul <raulvsh@gmail.com>
2022-02-16 21:10:08 +0100

    Desempaquetado de tuplas

--- video8.py
+++ video8.py
@@ -1,17 +1,26 @@
-miTupla=("Juan", 13,1,1995,13)
+miTupla = ("Juan", 13, 1, 1995)
 
 print(miTupla)
 
-miLista=list(miTupla)
+miLista = list(miTupla)
 
 print(miLista)
 
-miTupla2=tuple(miLista)
+miTupla2 = tuple(miLista)
 
 print(miTupla2)
 miLista.append(2)
 
 print(miLista)
 
-print ("Juan" in miTupla2)
-print (miTupla2.count(13))
+print("Juan" in miTupla2)
+print(miTupla2.count(13))
+
+
+nombre, dia, mes, anyo = miTupla2
+print("Nombre: " + nombre)
+print("Dia " + str(dia))
+print("Mes " + str(mes))
+print("Anyo " + str(anyo))
+
+print(miTupla2.index(13))
